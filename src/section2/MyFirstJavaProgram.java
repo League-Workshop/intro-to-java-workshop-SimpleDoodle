4d532c720d9ec39a26545a0900be8d4ff084b654
audreychan <league@WTS14-4.attlocal.net>
2019-03-07 18:28:37 -0800

    Eclipse

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,15 +1,21 @@
 package section2;
 
-import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
-		
-		
+		Robot walle = new Robot();
+		walle.setSpeed(10000);
+		walle.penDown();
+		for (int i = 0; i < 3; i++) {
+			walle.move(200);
+			walle.turn(120);
+
+		}
+
+		walle.penUp();
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,25 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
-		// 5. Set your robot's pen down 
-		
+		Robot walle = new Robot();
+		// 5. Set your robot's pen down
+		walle.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		walle.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
-			// 7. Change the pen color to random
-		
-			// 6. Move the robot 5 times the loop counter (5*i)
-			
-			// 2. Turn the robot 360/7 degrees to the right
-		
-			// 8. Set the pen width to i
-			
+		for (int i = 0; i < 75; i++) {
+			walle.setRandomPenColor();
+			walle.move(i * 5);
+			walle.turn(360 / 7);
+			walle.setPenWidth(i);
+		}
+		// 7. Change the pen color to random
+
+		// 6. Move the robot 5 times the loop counter (5*i)
+
+		// 2. Turn the robot 360/7 degrees to the right
+
+		// 8. Set the pen width to i
+
 	}
 }
