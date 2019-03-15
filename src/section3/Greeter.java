f345bb4566b1ed831ecdbd86a107489eb9e93ecf
audreychan <league@WTS14-4.attlocal.net>
2019-03-14 18:29:12 -0700

    Section 3

---
+++ src/section3/Greeter.java
@@ -0,0 +1,14 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+	public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"You are a good teacher");
+	JOptionPane.showMessageDialog(null,"You do not suck");
+	JOptionPane.showMessageDialog(null,"You are better than an average person");
+	JOptionPane.showMessageDialog(null,"On scale of 1 to 10 you are an 11");
+	JOptionPane.showMessageDialog(null,"Insert compliment here");
+}
+
+}
---
+++ src/section3/Snooper.java
@@ -0,0 +1,17 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Snooper {
+public static void main(String[] args) {
+	System.out.println("Welcome");
+	String name=JOptionPane.showInputDialog("What is your name?");
+	JOptionPane.showMessageDialog(null, name + " is legendary");
+	String color=JOptionPane.showInputDialog("What is your favorite color?");
+	JOptionPane.showMessageDialog(null, color + " is the best color");
+	String animal=JOptionPane.showInputDialog("What is your favorite animal?");
+	JOptionPane.showMessageDialog(null, color + " is the only color");
+	JOptionPane.showMessageDialog(null, "The best animal is the " + animal);
+	
+}
+}
