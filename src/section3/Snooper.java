cbcfdeedb18198ca2c88426a7de90da4ed1c690c
andrew983 <league@WTS14-4.attlocal.net>
2019-03-21 18:29:51 -0700

    Dragon Fight

--- src/section3/Snooper.java
+++ src/section3/Snooper.java
@@ -6,6 +6,8 @@ public class Snooper {
 public static void main(String[] args) {
 	System.out.println("Welcome");
 	String name=JOptionPane.showInputDialog("What is your name?");
+	
+	
 	JOptionPane.showMessageDialog(null, name + " is legendary");
 	String color=JOptionPane.showInputDialog("What is your favorite color?");
 	JOptionPane.showMessageDialog(null, color + " is the best color");
--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -7,21 +7,36 @@ import javax.swing.ImageIcon;
 public class DragonFight {
 	// 1. Create a main method and put all of the code below inside of it
 	public static void main(String[] args) {
-		
+		Random gen= new Random();
+		int a=gen.nextInt(11);
+		int b=gen.nextInt(26);
 		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");		
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+		int playerHealth=100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int dragonHealth=100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int playerDamage=20;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
-		
+		int dragonDamage=40;
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(playerHealth>0 && dragonHealth>0) { 
+			String attack=JOptionPane.showInputDialog(null, "Do you want to attack the dragon with a pokeball or shout?");
+	if(attack.equals("pokeball")) {
+		playerDamage=gen.nextInt(11);
+		dragonHealth=dragonHealth-playerDamage;
+		JOptionPane.showMessageDialog(null, "Your pokeball delt " +playerDamage+ " damage!");
+		JOptionPane.showMessageDialog(null,"The dragon has " +dragonHealth+ " health left.");
+	}
+	if(attack.equals("shout")) {
+		playerDamage=gen.nextInt(26);
+		dragonHealth=dragonHealth-playerDamage;
+	JOptionPane.showMessageDialog(null, "Your shout delt " +playerDamage+ " damage!");
+	JOptionPane.showMessageDialog(null,"The dragon has " +dragonHealth+ " health left.");
+	}
+	}
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
